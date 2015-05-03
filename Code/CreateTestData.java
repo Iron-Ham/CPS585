6e0d6dc3fb510c0d17bf07bcdf071d0e54f6c9df
Hesham Salman <hesham8@gmail.com>
2015-05-03 16:02:04 -0400

    Clustering DPNP & DPNT

--- Code/CreateTestData.java
+++ Code/CreateTestData.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 public class CreateTestData {
   public static void main(String[] args) throws FileNotFoundException {
-    PrintWriter test = new PrintWriter(new File("testing.csv"));
+    PrintWriter test = new PrintWriter(new File("../data/testing.csv"));
     Random rn = new Random();
 
+    test.println("Station, Station Name, Elevation, Latitude, Longitude, Date, DPNP, DPNT, DPT90, DX32,Normalized DPNT,Normalized Elevation,Normalized DPNP, DPNT Class, Elevation Class, DPNP Class, Normalized Latitude, Normalized Longitude, Latitude Class, Longitude Class");
     for (String s : args) {
       File f = new File(s);
       Scanner fileReader = new Scanner(f);
